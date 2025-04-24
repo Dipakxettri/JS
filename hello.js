d3399466ed3ee5ecbde81289ae12b38683c6ba77
Deepak Ghimire <139208927+Dipakxettri@users.noreply.github.com>
2025-04-24 13:25:28 +0545

    Update and rename file.py to hello.js

---
+++ hello.js
@@ -0,0 +1,16 @@
+colnsole.log("Hello Word");
+
+for(int i = 0; i < 10; i++){
+  console.log(i);
+}
+
+let j = 1;
+
+let func = ((array) => {
+  arry.forEach((element) => {
+    console.log(element);
+  })
+})
+
+let arry = [1,2,3,4];
+func(arry);
